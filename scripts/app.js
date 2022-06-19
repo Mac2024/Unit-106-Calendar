72467bbef630f411fd696c8535dbec5876828ff1
Mac2024 <99700613+Mac2024@users.noreply.github.com>
2022-06-19 10:49:39 -0500

    Assignment 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,74 @@
-function init() {
-    console.log("task manager");
+const nonImpIcon = "far fa-star";
+const impIcon = "fas fa-star";
+var isImportant = false;
+var isVisible = true;
 
-// load data
+function toggleImportant() {
+  console.log("Icon clicked!");
 
-// hook events
-
-
-
+  if (isImportant) {
+    $("#iImportant").removeClass(impIcon).addClass(nonImpIcon);
+  } else {
+    $("#iImportant").removeClass(nonImpIcon).addClass(impIcon);
+    isImportant = true;
+  }
+}
+function togglePanel() {
+  if (isVisible) {
+    $("#pnlForm").fadeOut();
+    isVisible = false;
+  } else {
+    $("#pnlForm").fadeIn();
+    isVisible = true;
+  }
 }
 
+function saveTask() {
+  console.log("Saving");
+  let title = $("#txtTitle").val();
+  let duration = $("#txtDuration").val();
+  let deadline = $("#selDeadline").val();
+  let location = $("#txtLocation").val();
+  let status = $("#selStatus").val();
+  let important = $("#iImportant").val();
+  console.log(title, duration, deadline, location, status, important);
 
+  let task = new Task(
+    0,
+    title,
+    duration,
+    deadline,
+    location,
+    status,
+    isImportant
+  );
+  console.log(task);
 
-window.onloaad = init;
+  displayTask(task);
+}
+function displayTask(task) {
+  let syntax = `<div class = 'task'>
+        <h3>${task.title}</h3>
+        <label>${task.location}</label>
+        <div class = 'dates'>
+            <label>${task.duration}</label>
+            <label>${task.deadline}</label>
+        </div>
+    </div>`;
+
+  $("#task-list").append(syntax);
+}
+
+function init() {
+  //runTests();
+  console.log("task manager");
+
+  // load data
+
+  // hook events
+  $("#iImportant").click(toggleImportant);
+  $("#btnShowHide").click(togglePanel);
+  $("#btnSave").click(saveTask);
+}
+
+window.onload = init;
---
+++ scripts/task.js
@@ -0,0 +1,16 @@
+
+
+
+class Task {
+    constructor (id,title, duration, deadline, location, status, important) {
+        this.id = id;
+        this.title = title;
+        this.duration= duration;
+        this.deadline = deadline;
+        this.location = location;
+        this.status = status;
+        this.important = important;
+
+        this.name ="Mario";
+    }
+}
