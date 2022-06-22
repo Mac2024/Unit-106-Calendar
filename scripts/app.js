8d25a43fc4e3e458c932857693b3a32d8c5c41f2
Mac2024 <99700613+Mac2024@users.noreply.github.com>
2022-06-22 12:36:58 -0500

    Competency

--- scripts/app.js
+++ scripts/app.js
@@ -32,7 +32,6 @@ function saveTask() {
   let status = $("#selStatus").val();
   let important = $("#iImportant").val();
   console.log(title, duration, deadline, location, status, important);
-
   let task = new Task(
     0,
     title,
@@ -43,32 +42,96 @@ function saveTask() {
     isImportant
   );
   console.log(task);
-
-  displayTask(task);
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+    type: "POST",
+    data: JSON.stringify(task),
+    contentType: "application/json",
+    success: function (response) {
+      let savedTask = JSON.parse(response); // parse json response into js opbject
+      displayTask(savedTask);
+    },
+    error: function (details) {
+      console.log("Error saving", details);
+    },
+  });
 }
+function getStatusText(status) {
+  switch (status) {
+    case 0:
+      return "New";
+    case 1:
+      return "In Progress";
+    case 3:
+      return "Blocked";
+    case 6:
+      return "Completed";
+    case 9:
+      return "Removed";
+    default:
+      return "missing";
+  }
+}
+
 function displayTask(task) {
-  let syntax = `<div class = 'task'>
+  let syntax = `<div class = 'task status-${task.status}'>
         <h3>${task.title}</h3>
         <label>${task.location}</label>
         <div class = 'dates'>
             <label>${task.duration}</label>
             <label>${task.deadline}</label>
+            <label>${getStatusText(task.status)}</label>
         </div>
     </div>`;
 
   $("#task-list").append(syntax);
 }
 
+function fetchTasks() {
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    type: "GET",
+    contentType: "",
+    success: function (response) {
+      let tasks = JSON.parse(response); //array of tasks
+      for (let i = 0; i < tasks.length; i++) {
+        let item = tasks[i];
+        if (item.name == "Mario") {
+          displayTask(item);
+        }
+      }
+
+      // for loop, get every object and send it to displayTask
+    },
+    error: function (dets) {
+      console.log("Error fetching tasks", dets);
+    },
+  });
+}
+function clearAllTasks() {
+  $.ajax({
+    type: "DELETE",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/clear/Mario",
+    success: function () {
+      $("#task-list").html("");
+    },
+    error: function (err) {
+      console.error(err);
+    },
+  });
+}
 function init() {
   //runTests();
   console.log("task manager");
 
   // load data
+  fetchTasks();
 
   // hook events
   $("#iImportant").click(toggleImportant);
   $("#btnShowHide").click(togglePanel);
   $("#btnSave").click(saveTask);
+  $("#delBtn").click(clearAllTasks);
 }
 
 window.onload = init;
